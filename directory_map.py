49de503d42c0534c0a4e186644a4425ec2d21152
Ahmed Arigbabu <64646510+medwonuola@users.noreply.github.com>
2023-03-02 17:55:41 +0100

    added file save

--- directory_map.py
+++ directory_map.py
@@ -1,17 +1,26 @@
 import os
 
 
-def print_folder_tree(directory, prefix='', dir_level=0):
+def folder_tree(directory, prefix='', dir_level=0, file=None):
     if (dir_level == 0):
-        print(directory.split('/')[-1] + '/')
+        folder_name = directory.split('/')[-1]
+        file.write(f"{folder_name}/\n")
     for item in os.listdir(directory):
         if item.startswith('.'):
             continue
         item_path = os.path.join(directory, item)
         if os.path.isdir(item_path):
-            print(f'{prefix}|--{item}/')
-            print_folder_tree(item_path, prefix=prefix+'|  ', dir_level=dir_level+1)
+            file.write(f'{prefix}|-- {item}/\n')
+            folder_tree(item_path, prefix=prefix+'|   ', dir_level=dir_level+1, file=file)
+
+
+def save_folder_tree(directory):
+    output_file = f"{directory.split('/')[-1]}_path.txt"
+    with open(output_file, 'w') as file:
+        folder_tree(directory, file=file)
+    print("Folder tree saved to ", output_file)
 
 
 if __name__ == '__main__':
-    print_folder_tree('/Users/mac/Projects/Contribution')
+    directory = '/Users/mac/Projects/Contribution'
+    save_folder_tree(directory)
