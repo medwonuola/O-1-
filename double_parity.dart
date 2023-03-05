8d4918014ac9432d2dc2c96a6bac930c9051336f
Ahmed Arigbabu <64646510+medwonuola@users.noreply.github.com>
2023-03-05 11:08:09 +0100

    renamed file

--- double_parity.dart
+++ double_parity.dart
@@ -1,4 +1,4 @@
-String getType(int x, int y) {
+String getParityofTwoNumbers(int x, int y) {
   String result = (x + y) % 2 == 0
       ? (x % 2 == 0 ? "Both are even" : "Only one is even")
       : "Both are odd";
