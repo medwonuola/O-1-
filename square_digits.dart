89fcfda6bf5bd3afc175f4cd59e6b18058886763
Ahmed Arigbabu <64646510+medwonuola@users.noreply.github.com>
2023-03-06 08:12:54 +0100

    another approach

--- square_digits.dart
+++ square_digits.dart
@@ -1,2 +1,11 @@
 int squareDigits(int n) => int.parse(
     n.toString().split('').map((e) => int.parse(e) * int.parse(e)).join());
+
+// Another approach:
+int sol2(int n) {
+  String sum = "";
+  n.toString().split('').forEach((e) {
+    sum += (int.parse(e) * int.parse(e)).toString();
+  });
+  return int.parse(sum);
+}
