63dd21ffcac0518ef0c0d7707e1a6dbc3329cdd7
Ahmed Arigbabu <64646510+medwonuola@users.noreply.github.com>
2023-03-06 08:25:46 +0100

    exes and ohs

---
+++ exes_ohs.dart
@@ -0,0 +1,18 @@
+bool xo(String arg) {
+  return arg.toLowerCase().split('x').length ==
+      arg.toLowerCase().split('o').length;
+}
+
+// earlier solution:
+bool xo2(String arg) {
+  int x = 0;
+  int o = 0;
+  arg.split('').forEach((e) {
+    if (e.toUpperCase() == 'X') {
+      x++;
+    } else if (e.toUpperCase() == 'O') {
+      o++;
+    }
+  });
+  return x == o;
+}
