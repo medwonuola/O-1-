8c63fffab323c782ef0d9732c27361ff360254c8
Ahmed Arigbabu <64646510+medwonuola@users.noreply.github.com>
2023-03-06 14:11:55 +0100

    changed to arrow function

--- double_parity.dart
+++ double_parity.dart
@@ -1,6 +1,3 @@
-String getParityofTwoNumbers(int x, int y) {
-  String result = (x + y) % 2 == 0
-      ? (x % 2 == 0 ? "Both are even" : "Only one is even")
-      : "Both are odd";
-  return result;
-}
+String getParityofTwoNumbers(int x, int y) => (x + y) % 2 == 0
+    ? (x % 2 == 0 ? "Both are even" : "Only one is even")
+    : "Both are odd";
