7a87226fd6e156099bdbe2c7ed5075c94c985dd0
Ahmed Arigbabu <64646510+medwonuola@users.noreply.github.com>
2023-03-06 08:07:15 +0100

    improve readability

--- digital_root.dart
+++ digital_root.dart
@@ -1,3 +1,14 @@
 int digitalRoot(int n) {
   return n < 10 ? n : digitalRoot(n ~/ 10 + n % 10);
 }
+
+// More readable:
+int sol2(int n) {
+  int sum = 0;
+
+  while (n > 0) {
+    sum += n % 10;
+    n = n ~/ 10;
+  }
+  return sum > 9 ? digitalRoot(sum) : sum;
+}
