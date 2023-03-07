abe59891d1ad79912a52529265ad676f5ec74b14
Ahmed Arigbabu <64646510+medwonuola@users.noreply.github.com>
2023-03-07 20:02:41 +0100

    approach 2

--- strong_number.dart
+++ strong_number.dart
@@ -10,3 +10,18 @@ String strong(int n) {
       ? "STRONG!!!!"
       : "Not Strong !!";
 }
+
+// approach 2:
+String sol(int n) {
+  int factorial(int n) => n <= 1 ? 1 : n *= factorial(n - 1);
+  int _n = n;
+  int sum = 0;
+
+  while (_n > 0) {
+    int digit = _n % 10;
+    sum += factorial(digit);
+    _n ~/= 10;
+  }
+
+  return sum == n ? "STRONG!!!!" : "Not Strong !!";
+}
